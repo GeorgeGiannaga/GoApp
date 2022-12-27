17abfee901d3398e20c5c9d03eab202936f001e6
George Giannakopoulos <75226733+GeorgeGiannaga@users.noreply.github.com>
2022-12-27 20:38:55 +0200

    Add files via upload

---
+++ goapp/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func ReadFile(filename string) []string {
+	words := []string{}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	return words
+}
+
+func main() {
+	var word string
+	filename := "dat.txt"
+	fmt.Println("Please specify the word , that you want to be counted in the text of the file")
+	fmt.Scan(&word)
+	text := ReadFile(filename)
+	var j int
+	for i := 0; i < len(text); i++ {
+		if text[i] == string(word) {
+			j++
+			break
+		}
+	}
+	if j == 0 {
+		fmt.Printf("There is no such word found in the text")
+	} else {
+		fmt.Printf("'%s' word has been identified in the next %v time/s", word, j)
+	}
+}
